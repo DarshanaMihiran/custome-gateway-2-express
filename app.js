9fd9acaa1dcd20a2557c3e3025997bf14c31c181
Darshana Edirisinghe <mihiranlumiaa@outlook.com>
2024-05-17 09:04:20 +0530

    enable ssl

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const https = require('https');
+const fs = require('fs');
 
 app.get('/api', (req, res) => {
-    res.send('Hello');
+    res.send('Hello, This is the Express Gateway with TLS/SSL');
 })
 
-app.listen(port, () => {
+const options = {
+    key: fs.readFileSync('./https/private-key.pem'),
+    cert: fs.readFileSync('./https/certificate.pem'),
+};
+
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
     console.log(`Edge gateway is running on http://localhost:${port}`);
 });
